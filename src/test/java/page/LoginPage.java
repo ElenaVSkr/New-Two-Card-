d16cc0ed759d608476d765fdc5c0fdc452ac0e67
Elena S <prostovael@yandex.ru>
2024-07-31 17:33:52 +0400

    .github/

---
+++ src/test/java/page/LoginPage.java
@@ -0,0 +1,18 @@
+package page;
+import com.codeborne.selenide.SelenideElement;
+import data.DataHelper;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class LoginPage {
+    private final SelenideElement loginFiled = $("[data-test-id=login] input");
+    private final SelenideElement passwordFiled = $("[data-test-id=password] input");
+    private final SelenideElement loginButton = $("[data-test-id=action-login]");
+
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
+        loginFiled.setValue(info.getLogin());
+        passwordFiled.setValue(info.getPassword());
+        loginButton.click();
+        return new VerificationPage();
+    }
+}
